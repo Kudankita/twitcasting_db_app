703ef11a6af3a15e372e34bb2b1479ea63fc5cff
Silver <41056212+Kudankita@users.noreply.github.com>
2019-06-01 11:21:24 +0900

    rails tutorial7章相当、ログインユーザをDBに登録することまで完了

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -1,4 +1,31 @@
+# frozen_string_literal: true
+
 class DevelopersController < ApplicationController
   def new
+    @developer = Developer.new
+    # if @developer.save
+    #   # Handle a successful save.
+    # else
+    #   render 'new'
+    # end
+  end
+
+  def create
+    @developer = Developer.new(user_params)
+    if @developer.save
+      # TODO: flashが必要かも含めて遷移先を今後変更する
+      # Handle a successful save.
+      flash[:success] = 'Welcome to the Sample App!'
+      render 'new'
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:developer).permit(:name, :email, :password,
+                                      :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'developers/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'members' => 'members#index'
   get 'users' => 'users#index'
   get 'users/new' => 'users#new'
   post 'users' => 'users#create'
+  get 'signup' => 'developers#new'
+  resources :developers
 end
