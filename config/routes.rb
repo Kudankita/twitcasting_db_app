6b45bab2be9f0e15752f1a303f485dedc7f0cb34
Silver <41056212+Kudankita@users.noreply.github.com>
2019-06-09 18:56:33 +0900

    ログインしていないとアクセスできないように処理を追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@
 Rails.application.routes.draw do
   get 'sessions/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'members' => 'members#index'
+  # get 'members' => 'members#index'
   get 'users' => 'users#index'
   get 'users/new' => 'users#new'
   post 'users' => 'users#create'
   get 'signup' => 'developers#new'
-  get    'login'   => 'sessions#new'
-  post   'login'   => 'sessions#create'
-  delete 'logout'  => 'sessions#destroy'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :developers
 end
